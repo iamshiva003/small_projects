ea7f8043585559d612270a99a7f2a3146acb086e
vignesh-hegde <vignesh.hegde.07@gmail.com>
2026-02-22 21:13:22 +0530

    Added DB operations

---
+++ account_database.c
@@ -0,0 +1,136 @@
+#include "account_database.h"
+
+account_databse_header db_header;
+FILE *db_file_ptr;
+account_entry_t account_entry;
+
+db_operation_status load_databse()
+{
+    db_operation_status ret_status = DB_OPERATION_SUCCESS;
+    size_t write_count = 0;
+    db_customer_t temp_load[MAX_ALLOWED_ACCOUNT];
+
+    if((db_file_ptr = fopen(DB_NAME, "rb")) == NULL)
+    {
+        if((db_file_ptr = fopen(DB_NAME, "wb")) == NULL)
+        {
+            ret_status = DB_OPERATION_FAILED;
+            printf("E1\n");
+        }
+        else
+        {
+            db_header.account_count = 0;
+            db_header.db_signature = DB_SIGNATURE;
+            if(fwrite(&db_header, sizeof(db_header), 1, db_file_ptr) != 1)
+            {
+                ret_status = DB_OPERATION_FAILED;
+                printf("E2\n");
+            }
+        }
+    }
+    else
+    {
+        if(fread(&db_header, sizeof(db_header), 1, db_file_ptr) != 1)
+        {
+            ret_status = DB_OPERATION_FAILED;
+            printf("E3\n");
+        }
+        else if (db_header.db_signature != DB_SIGNATURE)
+        {
+            ret_status = DB_OPERATION_FAILED;
+            printf("E4\n");
+        }
+    }
+
+    // load db onto account entry
+    if((ret_status & DB_FAIL_MASK) != DB_OPERATION_FAILED)
+    {
+        account_entry.count = db_header.account_count;
+        printf("HEAD C : %d\n", db_header.account_count);
+        printf("HEAD S : %X\n", db_header.db_signature);
+
+        if((account_entry.entry = (customer_t *)calloc(MAX_ALLOWED_ACCOUNT, sizeof(customer_t))) == NULL)
+        {
+            ret_status = DB_OPERATION_FAILED;
+            printf("E5\n");
+        }
+        else
+        {
+            size_t temp = fread(temp_load, sizeof(db_customer_t), account_entry.count, db_file_ptr);
+            if( temp != account_entry.count)
+            {
+                printf(">>>> %d\n", temp);
+                ret_status = DB_OPERATION_FAILED;
+                printf("E6\n");
+            }
+            else
+            {
+                for(uint32_t i = 0; i < account_entry.count; i++)
+                {
+                    strncpy(account_entry.entry[i].first_name, temp_load[i].first_name, CHAR_FIELD_LENGTH);
+                    strncpy(account_entry.entry[i].last_name, temp_load[i].last_name, CHAR_FIELD_LENGTH);
+                    strncpy(account_entry.entry[i].father_name, temp_load[i].father_name, CHAR_FIELD_LENGTH);
+                    account_entry.entry[i].age = temp_load[i].age;
+                    account_entry.entry[i].cbalance = temp_load[i].cbalance;
+                    account_entry.entry[i].aadhar = temp_load[i].aadhar;
+                    strncpy(account_entry.entry[i].DOB, temp_load[i].DOB, 11);
+                    strncpy(account_entry.entry[i].place, temp_load[i].place, CHAR_FIELD_LENGTH);
+                }
+            }
+        }
+    }
+
+    fclose(db_file_ptr);
+
+    return ret_status;
+}
+
+db_operation_status save_databse()
+{
+    printf("SDB!!!\n");
+
+    db_operation_status ret_status = DB_OPERATION_SUCCESS;
+    db_customer_t temp_load[MAX_ALLOWED_ACCOUNT];
+
+    if((db_file_ptr = fopen(DB_NAME, "wb")) == NULL)
+    {
+        ret_status = DB_OPERATION_FAILED;
+        printf("SDB: E1\n");
+    }
+    else
+    {
+        db_header.account_count = account_entry.count;
+        printf("%d %d\n",db_header.account_count, account_entry.count);
+
+        if(fwrite(&db_header, sizeof(db_header), 1, db_file_ptr) == 1)
+        {
+            for(uint32_t i = 0; i < account_entry.count; i++)
+            {
+                printf("PUTTING DATA %d\n", i);
+                strncpy(temp_load[i].first_name, account_entry.entry[i].first_name, CHAR_FIELD_LENGTH);
+                strncpy(temp_load[i].last_name, account_entry.entry[i].last_name, CHAR_FIELD_LENGTH);
+                strncpy(temp_load[i].father_name, account_entry.entry[i].father_name, CHAR_FIELD_LENGTH);
+                temp_load[i].age = account_entry.entry[i].age;
+                temp_load[i].cbalance = account_entry.entry[i].cbalance;
+                temp_load[i].aadhar = account_entry.entry[i].aadhar;
+                strncpy(temp_load[i].DOB, account_entry.entry[i].DOB, 11);
+                strncpy(temp_load[i].place, account_entry.entry[i].place, CHAR_FIELD_LENGTH);
+                printf("PUTTING DATA %d END\n", i);
+            }
+
+            printf("account_entry.count >> %d\n", account_entry.count);
+            if(fwrite(temp_load, sizeof(db_customer_t), account_entry.count, db_file_ptr) != account_entry.count)
+            {
+                ret_status = DB_OPERATION_FAILED;
+                printf("SDB: E2\n");
+            }
+        }
+        else
+        {
+            ret_status = DB_OPERATION_FAILED;
+            printf("SDB: E3\n");
+        }
+    }
+
+    fclose(db_file_ptr);
+}
--- bms.c
+++ bms.c
@@ -1,10 +1,20 @@
 #include "bms.h"
+customer_t *account;
 
+void wrapper(void (*ops_func_ptr[2])(customer_t *), customer_t *customer_data)
+{
+    ops_func_ptr[0](customer_data);
+    ops_func_ptr[1](customer_data);
+}
 
 bms_proc_t bms_init()
 {
-    bms_proc_t ret_status = bms_proc_success;
-
+    bms_proc_t ret_status = (load_databse()==DB_OPERATION_FAILED) ? bms_proc_failed : bms_proc_success;
+    
+    if(ret_status == bms_proc_failed)
+    {   
+        printf("FAILED TO LOAD DATABSE!\n");
+    }
     return ret_status;
 }
 
@@ -13,16 +23,35 @@ bms_proc_t bms_exec()
     static int run_flag = 1;
 
     bms_proc_t ret_status = bms_proc_success;
-    void (*menu[])(customer_t *) = {deposit_menu, create_menu, withdraw_menu, account_deatils_menu, save_and_exit_menu};
+
+    // ORDER MATTERS ;-)
+    void (*operation_func[5][2])(customer_t *) = {
+        {create_menu, create_account},
+        {deposit_menu, deposit_amount},
+        {withdraw_menu, withdraw_amount},
+        {account_deatils_menu, account_details},
+        {save_and_exit_menu, NULL}};
 
     main_menu_opt_t main_menu_option;
     customer_t customer_data_holder;
 
     while(run_flag)
     {
-        main_menu_option = main_menu();
-        (main_menu_option == INVALID_MENU) ? printf("INVALID OPTION SELECTED\n") : (menu[main_menu_option])(&customer_data_holder);
+        if((main_menu_option = main_menu()) == SAVE_AND_EXIT)
+        {
+            run_flag = 0;
+            bms_exit();
+        }
+        else
+        {
+            (main_menu_option == INVALID) ? printf("INVALID OPTION SELECTED\n") : wrapper(operation_func[main_menu_option], &customer_data_holder);
+        }
     }
     
     return ret_status;
+}
+
+void bms_exit()
+{
+    save_databse();
 }
